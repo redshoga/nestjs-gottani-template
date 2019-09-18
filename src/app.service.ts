7366b6a7975118cf077bb2131a4a8aff82ea4c94
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-09-18 16:18:15 +0900

    Add dotenv setting

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from './config/config.service';
 
 @Injectable()
 export class AppService {
+  private sampleKey: string;
+  constructor(config: ConfigService) {
+    // this.sampleKey = config.get('SAMPLE_KEY');
+    this.sampleKey = config.sampleKey;
+  }
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! ' + this.sampleKey;
   }
 }
---
+++ src/config/config.service.ts
@@ -0,0 +1,44 @@
+import * as dotenv from 'dotenv';
+import * as Joi from '@hapi/joi';
+import * as fs from 'fs';
+
+export interface EnvConfig {
+  [key: string]: string;
+}
+
+export class ConfigService {
+  private readonly envConfig: EnvConfig;
+
+  constructor(filePath: string) {
+    const config = dotenv.parse(fs.readFileSync(filePath));
+    this.envConfig = this.validateInput(config);
+  }
+
+  /**
+   * Ensures all needed variables are set, and returns the validated JavaScript object
+   * including the applied default values.
+   */
+  private validateInput(envConfig: EnvConfig): EnvConfig {
+    const envVarsSchema: Joi.ObjectSchema = Joi.object({
+      SAMPLE_KEY: Joi.string().required(),
+    });
+
+    const { error, value: validatedEnvConfig } = envVarsSchema.validate(
+      envConfig,
+    );
+    if (error) {
+      throw new Error(`Config validation error: ${error.message}`);
+    }
+    return validatedEnvConfig;
+  }
+
+  // Generic getter method
+  get(key: string): string {
+    return this.envConfig[key];
+  }
+
+  // Custom getter functions
+  get sampleKey(): string {
+    return this.envConfig.SAMPLE_KEY;
+  }
+}
