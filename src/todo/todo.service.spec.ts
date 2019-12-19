e6ba13cbc8d919b3a189485c35c02a18ba27105e
Yui Ichioka <ichioka.yui@fixer.co.jp>
2019-12-19 17:14:37 +0900

    Fix tests

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,11 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Todo } from './todo.entity';
 
 describe('TodoService', () => {
   let service: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          ...require('../../ormconfig.test.js'),
+          entities: [
+            Todo,
+          ],
+        } as TypeOrmModuleOptions),
+        TypeOrmModule.forFeature([Todo]),
+      ],
       providers: [TodoService],
     }).compile();
 
